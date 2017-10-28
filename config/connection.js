fae2b4026951e42776ac70bfeb845c45b10f89fa
Alison Lawyer <alison@Alisons-Air.att.net>
2017-10-27 21:11:11 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 
 const mysql = require('mysql');
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
